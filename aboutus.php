597c3f1405bea3fdc0fdebc2270adc658b95d64f
aavail007 <aavail007@gmail.com>
2016-11-12 14:40:28 +0800

    jomor

---
+++ aboutus.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>桌末狂歡 JOMOR - 桌遊資訊平台</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="javascript.js"></script>
+	<meta charset="utf-8">
+	<link rel="icon" href="jomor_html/img/jomorparty_logo.png" type="image/ico" />
+</head>
+	<body id="body0">
+		<?php
+			include('include/link.php');
+			include('include/sessionCheck.php');
+			include('include/header.php'); 
+		?>
+		<section class="about_blue_section"><!--藍底區塊-->
+			<div class="about_blue_bg"><!--團隊介紹和信箱-->
+				<img class="aboutus_img" src="jomor_html/img/aboutus_img.png">
+				<p class="aboutus_p">我們是一群熱愛桌遊的大學生，希望能透過我們所學的專業，將桌遊的益處推廣出去，並開發新的桌遊資訊平台，其中包括桌遊店家的詳細資訊與地圖、揪團功能、桌遊專欄、討論區等，希望提供多方面的桌遊資訊平台，讓桌遊變得更平易近人，因而吸引更多玩家加入美好的桌遊世界。
+				</p>
+				<p class="aboutus_p2">網站上使用有任何問題請寄信至<a class="mail" href="mailto:ics.jomorparty@gmail.com">ics.jomorparty@gmail.com</a>
+				</p>
+			</div>
+		</section>
+		<section>
+			<div class="aboutall">
+				<img src="jomor_html/img/aboutusall.png" width="500px" height="1300px">
+			</div>
+		</section>
+</body>
+</html>
---
+++ discussion.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>桌末狂歡 JOMOR - 桌遊資訊平</title>
+	<link href="style.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="javascript.js"></script>
+	<meta charset="utf-8">
+	<link rel="icon" href="jomor_html/img/jomorparty_logo.png" type="image/ico" />
+</head>
+	<body id="body0">
+		<?php
+			include('include/link.php');
+			include('include/sessionCheck.php');
+			include('include/header.php'); 
+		?>
+		<img src="jomor_html/img/discussion.jpg" class="discussion_img">
+	</body>
+</html>
